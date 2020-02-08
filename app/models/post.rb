68e27d19034af2edd16c8da93a0c4fd50c94fb86
hironakazuki <43399066+hironakazuki@users.noreply.github.com>
2020-02-09 00:21:40 +0900

    Validation post title (#17)
    * add title validations
    
    * add content validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   has_rich_text :content
 
-  validates :title, presence: true
+  validates :title, presence:true, length: { maximum: 32 }
   validates :content, presence: true
 end
