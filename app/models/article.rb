8f6df40e723a5d3867617e5985b40bf50a278220
hironakazuki <far_east_nightbird30@icloud.com>
2020-02-20 17:58:51 +0900

    [change]content_attachment_validationの共通化

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,8 @@
 class Article < ApplicationRecord
   has_rich_text :content
+
+  validates :title, presence:true, length: { maximum: 32 }
+  validates :content, presence: true
+  
+  include ContentAttachmentValidators
 end
