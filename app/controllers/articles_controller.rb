8da2e51b160ff38b91b4b6ec2d75ecb5b9347334
hironakazuki <far_east_nightbird30@icloud.com>
2020-02-20 15:44:31 +0900

    [add]comparison_controllerの追加
    jobの有無での比較するため２つのモデルのindexを表示させるcomparison_controllerを追加しました。

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,9 +3,9 @@ class ArticlesController < ApplicationController
 
   # GET /articles
   # GET /articles.json
-  def index
-    @articles = Article.all
-  end
+  # def index
+  #   @articles = Article.all
+  # end
 
   # GET /articles/1
   # GET /articles/1.json
